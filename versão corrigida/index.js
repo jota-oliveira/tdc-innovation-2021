2ac5d49edcb5e6108140ee6dd4094094cbda293d
João Henrique de Oliveira Júnior <joao.holiveira@totvs.com.br>
2021-03-20 12:11:05 -0300

    Leitor de tela atualizações modal

--- versão corrigida/index.js
+++ versão corrigida/index.js
@@ -1,31 +1,12 @@
-const $ = document.querySelector.bind(document);
+import { ModalReportarProblema } from './modalReportarProblema.js';
 
-const modalContainer = $('#modalReportarProblema');
-const botaoFecharModal = $('#botaoFechar');
-
-const teclasPadraoAcoes = ['Space', 'Enter'];
-
-const abrirModal = () => {
-  modalContainer.classList.add('mostrar');
-  focarBotaoFecharModal();
-};
-
-const fecharModal = () => {
-  modalContainer.classList.remove('mostrar');
-}
-
-const focarBotaoFecharModal = () => {
-  botaoFecharModal.focus();
-}
-
-const reportarProblema = () => {
-  
-}
-
-const verificarBotaoPrecionado = (eventKeyDown) => {
-  if (teclasPadraoAcoes.includes(eventKeyDown.code)) {
-    eventKeyDown.preventDefault();
-    fecharModal();    
-  }
-}
-
+new ModalReportarProblema({
+  modal: '#modalReportarProblema',
+  btnFechar: '#btnFechar',
+  btnCancelar: '#btnCancelar',
+  btnEnviar: '#btnEnviar',
+  btnAbrir: '#btnReportarProblema',
+  input: '#descrevaSeuProblemaInput',
+  reportErro: '#mensagemErroModal',
+  btnMensagemBotaoEnviar: "#mensagemBotao",
+});
---
+++ versão corrigida/modalReportarProblema.js
@@ -0,0 +1,114 @@
+export class ModalReportarProblema {
+
+  formErrors = {
+    error: null
+  };
+
+  get teclasPadraoAcoes() {
+    return ['Space', 'Enter'];
+  }
+
+  constructor(referencias) {
+    this.$ = document.querySelector.bind(document);
+    this.modal = this.$(referencias.modal);
+    this.btnFechar = this.$(referencias.btnFechar);
+    this.btnCancelar = this.$(referencias.btnCancelar);
+    this.btnEnviar = this.$(referencias.btnEnviar);
+    this.btnAbrir = this.$(referencias.btnAbrir);
+    this.input = this.$(referencias.input);
+    this.reportErro = this.$(referencias.reportErro);
+    this.btnMensagemBotaoEnviar = this.$(referencias.btnMensagemBotaoEnviar);
+    this.ultimoElemento = this.btnEnviar;
+
+    this._escutarTeclaPadraoFechar();
+    this._prepararElementos();
+  }
+
+  abrir = () => {
+    this.modal.classList.add('mostrar');
+    this.focarBotaoFechar();
+  };
+
+  fechar = (event) => {
+    event.preventDefault();
+    this.modal.classList.remove('mostrar');
+  }
+
+  focarBotaoFechar = () => {
+    this.btnFechar.focus();
+  }
+
+  reportarProblema = (event) => {
+    event.preventDefault();
+
+    if (this._validarInput()) {
+      alert('Dados enviados!');
+    }
+  }
+
+  verificarBotaoPrecionado = (eventKeyUp) => {
+    if (this.teclasPadraoAcoes.includes(eventKeyUp.code)) {
+      this.fechar(eventKeyUp);    
+    }
+  }
+
+  _validarInput = () => {
+    try {
+      this.formErrors.error = null;
+      this._inputNaoVazio();
+      this._atualizarMensagemErro();
+    } catch (error) {
+      this.formErrors.error = error.message;
+      this._atualizarMensagemErro();
+      return false;
+    }
+
+    return true;
+  }
+
+  _inputNaoVazio = () => {
+    if (!this.input.value) {
+      throw new Error('Esse campo precisa ser preenchido');
+    }
+  }
+
+  _atualizarMensagemErro = () => {
+    this.reportErro.innerHTML = this.formErrors.error;
+
+    if (this.formErrors.error) {
+      this.reportErro.setAttribute("aria-live", "polite");
+      this.reportErro.setAttribute('tabindex', 0);
+      this.reportErro.focus();
+      this.btnEnviar.setAttribute("aria-disabled", true);
+      this.btnMensagemBotaoEnviar.innerHTML = 'Botão desabilitado porque o formulário está inválido';
+    } else {
+      this.btnCancelar.focus();
+      this.reportErro.removeAttribute('tabindex');
+      this.btnEnviar.setAttribute("aria-disabled", false);
+      this.btnMensagemBotaoEnviar.innerHTML = null;
+    }
+  }
+
+  _corrigirNavegacaoModal = () => {
+    this.ultimoElemento.onblur = this.focarBotaoFechar.bind(this);
+  }
+
+  _escutarTeclaPadraoFechar = () => {
+    document.addEventListener('keyup', (keyUpEvent) => {
+      if (keyUpEvent.code === 'Escape') {
+        this.fechar(keyUpEvent);
+      }
+    });
+  }
+
+  _prepararElementos = () => {
+    this.btnAbrir.onclick = this.abrir.bind(this);
+    this.btnFechar.onclick = this.fechar.bind(this);
+    this.btnFechar.onkeyup = this.verificarBotaoPrecionado.bind(this);
+    this.btnCancelar.onclick = this.fechar.bind(this);
+    this.btnEnviar.onclick = this.reportarProblema.bind(this);
+    this.input.onblur = this._validarInput.bind(this);
+
+    this._corrigirNavegacaoModal();
+  }
+}
